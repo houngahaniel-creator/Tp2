2c4c2063c366605d348f64f3bb1e37fd0ecd12cb
graciasproject <deograciasguidigan@gmail.com>
2025-11-28 08:03:31 +0100

    Add entrypoint script for Laravel application
    This script clears Laravel caches and starts PHP-FPM and Nginx.

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Vider les caches Laravel
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
+# Lancer PHP-FPM et Nginx
+php-fpm -D
+nginx -g "daemon off;"
+v
